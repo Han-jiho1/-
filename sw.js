3df17143a5e61fdef0dfdc03906903db77df09fc
Han-jiho1 <zpfhxk@naver.com>
2026-06-24 17:01:59 +0900

    Update cache version to quote-pwa-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quote-pwa-v1';
+const CACHE = 'quote-pwa-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
